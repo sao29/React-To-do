15a6a2d4714090240ec03d17bda99198bc5fac38
Salvador Alvarez <sao.95@hotmail.com>
2022-04-29 21:20:20 -0700

    agregar el hook de useState

--- todo/src/TodoSearch.jsx
+++ todo/src/TodoSearch.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import  './TodoSearch.css';
+import { useState } from 'react';
 
 function TodoSearch() {
-  const cambiosEvento = (event)=>{
-    console.log(event.target.value);
+
+  const [cambio, setCambio] = useState("");
+
+  // const cambiosEvento = (event)=>{
+  //   console.log(event.target.value);
+  //   setCambio(event.target.value)
+  // }
+  const cambiosEvento = (e)=> {
+     setCambio(e.target.value)
   }
+
   return (
-    <input 
-    onChange={cambiosEvento}
-    placeholder='escribe algo'
-    className="TodoSearch" />
+    <>
+    
+
+    <input
+  className="TodoSearch"
+  placeholder="Ingresa algo"
+  value={cambio}
+	onChange={cambiosEvento}
+      />
+    <p> {cambio} </p>
+    
+    </>
+   
   )
 }
 
