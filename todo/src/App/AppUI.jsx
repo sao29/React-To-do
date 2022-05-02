2fbd94fea4c1792babe9a3eded3f8e8991f0467f
Salvador Alvarez <sao.95@hotmail.com>
2022-05-02 15:53:38 -0700

    agregar el modal para agregar tareas

--- todo/src/App/AppUI.jsx
+++ todo/src/App/AppUI.jsx
@@ -4,48 +4,42 @@ import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButtom';
+import { TodoContext } from '../TodoContext';
 
-function AppUI({
-  loading,
-  error,
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+function AppUI(){
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+  } = React.useContext(TodoContext);
+  
   return (
     <React.Fragment>
-    <TodoCounter
-      total={totalTodos}
-      completed={completedTodos}
-    />
-    <TodoSearch
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-    />
+      <TodoCounter />
+      <TodoSearch />
 
-    <TodoList>
-      {error && <p>Desespérate, hubo un error...</p>}
-      {loading && <p>Estamos cargando, no desesperes...</p>}
-      {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
-      
-      {searchedTodos.map(todo => (
-        <TodoItem
-          key={todo.text}
-          text={todo.text}
-          completed={todo.completed}
-          onComplete={() => completeTodo(todo.text)}
-          onDelete={() => deleteTodo(todo.text)}
-        />
-      ))}
-    </TodoList>
+      <TodoList>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer TODO!</p>}
+        
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
 
-    <CreateTodoButton />
-  </React.Fragment>
-);
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
 }
 
 export { AppUI };
